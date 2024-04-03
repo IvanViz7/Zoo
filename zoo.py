d01db50f11b0870eb5846bf11b1be45131783eaf
FredyVizcarra <fredyvizcarrag@gmail.com>
2024-04-02 23:14:03 -0600

    Create Zoo and Habitat classes and delete statics animals classes

--- zoo.py
+++ zoo.py
@@ -1,3 +1,38 @@
+class Zoo:
+    def __init__(self):
+        self.habitats = []
+    
+    def add_habitat(self, habitat):
+        self.habitats.append(habitat)
+    
+    def add_animal(self, animal):
+        print("Select a habitat to add the animal to:")
+        for i, habitat in enumerate(self.habitats):
+            print(f"{i+1}. {habitat.name}")
+        habitat_index = int(input("Enter the habitat number: ")) - 1
+        self.habitats[habitat_index].add_animal(animal)
+    
+    def display_info(self):
+        print(f'Guadalajara Zoo:')
+        if not self.habitats:
+            print("* Empty zoo *")
+        else:
+            for habitat in self.habitats:
+                habitat.display_info()
+
+class Habitat:
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+    
+    def add_animal(self, animal):   
+        self.animals.append(animal)
+    
+    def display_info(self):
+        print(f'Habitat: {self.name}')
+        for animal in self.animals:
+            animal.info()
+
 class Animal:
     def __init__(self, name, type, specie, weight):
         self.name = name
@@ -14,179 +49,47 @@ class Animal:
     def info(self):
         print(f'The name of the {self.specie} is {self.name}, {self.name} is a {self.type} and it weights {self.weight} lb')
         
-zoo = []
-listchoice =[]
-        
-class Lion(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
+zoo = Zoo()
 
-    def move(self):
-        print(f'{self.name} is running')
+def get_user_choice():
+    while True:
+        zoo.display_info()
+        print("What would you like to add?")
+        print("1. Animal")
+        print("2. Habitat")
+        choice = input("Enter your choice (1 or 2): ")
+        if choice == "1":
+            while True:
+                print("What type of animal would you like to add?")
+                print("1. Reptile")
+                print("2. Mammal")
+                print("3. Fish")
+                animal_type_choice = input("Enter your choice (1, 2 or 3): ")
+                if animal_type_choice in ["1", "2", "3"]:
+                    return choice, animal_type_choice
+                else:
+                    print("Invalid choice. Please enter 1, 2 or 3.")
+        elif choice == "2":
+            return choice, None
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+while True:
+    user_choice = get_user_choice()
+    if user_choice == "1":
+        name = input("Enter the name of the animal: ")
+        animal_type = input("Enter the type of the animal: ")
+        species = input("Enter the species of the animal: ")
+        weight = float(input("Enter the weight of the animal: "))
+        new_animal = Animal(name, animal_type, species, weight)
+        zoo.add_animal(new_animal)
+    elif user_choice == "2":
+        habitat_name = input("Enter the name of the habitat: ")
+        new_habitat = Habitat(habitat_name)
+        zoo.add_habitat(new_habitat)
     
-    def eat(self):
-        print(f'{self.name} is eating meat')
-        
-class Monkey(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
+    another_action = input("Do you want to add another animal or habitat? (yes/no): ")
+    if another_action.lower() != "yes":
+        break
 
-    def move(self):
-        print(f'{self.name} is jumping between ropes')
-    
-    def eat(self):
-        print(f'{self.name} is eating fleas')
-
-class Iguana(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is running over the water')
-    
-    def eat(self):
-        print(f'{self.name} is eating vegetables and fruits')
-
-class Crocodile(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is swimming in the water')
-    
-    def eat(self):
-        print(f'{self.name} is eating meat')
-        
-class Zebra(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is jumping in the field')
-    
-    def eat(self):
-        print(f'{self.name} is eating grass')
-
-class Giraffe(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is running around the field')
-    
-    def eat(self):
-        print(f'{self.name} is eating grass from the trees')
-        
-class Turtle(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is sleeping in its shell')
-    
-    def eat(self):
-        print(f'{self.name} is eating seaweed and shrimp')
-        
-class Hippo(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is taking a shower in the lake')
-    
-    def eat(self):
-        print(f'{self.name} is eating lettuce')
-        
-class Python(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is trying to hide in its cave')
-    
-    def eat(self):
-        print(f'{self.name} is eating meat')
-        
-class Tigger(Animal):
-    def __init__(self, name, type, specie, weight):
-        super().__init__(name, type, specie, weight)
-
-    def move(self):
-        print(f'{self.name} is running too quickly')
-    
-    def eat(self):
-        print(f'{self.name} is eating meat')
-
-lion = Lion('Jackson', 'Mammal', 'Lion', 600)
-monkey = Monkey('Lisa', 'Mammal', 'Monkey', 80)
-iguana = Iguana('Iguanol', 'Reptile', 'Iguana', 6)
-crocodile = Crocodile('Pancho', 'Reptile', 'Crocodile', 450)
-zebra = Zebra('Marty', 'Mammal', 'Zebra', 700)
-giraffe = Giraffe('Melman', 'Mammal', 'Giraffe', 1000)
-turtle = Turtle('Leonardo', 'Reptile', 'Turtle', 800)
-hippo = Hippo('Maria', 'Mammal', 'Hippo', 1800)
-python = Python('Pedro', 'Reptile', 'Snake', 30)
-tigger = Tigger('Gatito', 'Mammal', 'Tigger', 450)
-
-def add_animal(animal):
-    zoo.append(animal)
-    animals_zoo=len(zoo)
-    if(animals_zoo < 10):
-        print(f"{animal.name} the {animal.specie} now is a member of our Zoo family")
-        animal.info()
-        animal.move()
-        animal.eat() 
-
-        
-while len(zoo) < 10:
-    choice = input('Choose a number form 1 to 10 to add an animal to the Zoo: ')
-    choice = int(choice)
-    newchoice = choice
-    
-    if choice < 1 or choice > 10:
-        print('Invalid character')
-    elif newchoice in listchoice:
-        print('This animal is already in the Zoo')
-    else:
-        if(choice == 1):
-            animal = lion
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 2):
-            animal = monkey
-            add_animal(animal)  
-            listchoice.append(newchoice)
-        elif(choice == 3):
-            animal = iguana
-            add_animal(animal)            
-            listchoice.append(newchoice)
-        elif(choice == 4):
-            animal = crocodile
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 5):
-            animal = giraffe
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 6):
-            animal = zebra
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 7):
-            animal = turtle
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 8):
-            animal = hippo
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 9):
-            animal = python
-            add_animal(animal)
-            listchoice.append(newchoice)
-        elif(choice == 10):
-            animal = tigger
-            add_animal(animal)
-            listchoice.append(newchoice)
-else:
-    print('The Zoo is full')
+zoo.display_info()
