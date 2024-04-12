188d5fd3d426696821e996da149d95f437f2961a
FredyVizcarra <fredyvizcarrag@gmail.com>
2024-04-12 00:43:55 -0600

    Delete get_user_choice from zoo.py and added to zoo class file

--- module_zoo.py
+++ module_zoo.py
@@ -28,6 +28,39 @@ class Zoo:
         else:
             print(f"\nCannot add {animal.name} to {habitat.name}. Incompatible animal types.")
     
+    def get_user_choice(self):
+        while True:
+            self.display_info()
+            print("\nWhat would you like to add?")
+            print("1. Animal")
+            print("2. Habitat")
+            choice = input("\nEnter your choice (1 or 2): ")
+            if choice == "1":
+                print("\nWhat type of animal would you like to add?")
+                print("1. Reptile")
+                print("2. Mammal")
+                print("3. Fish")
+                animal_type_choice = input("\nEnter your choice (1, 2 or 3): ")
+                if animal_type_choice in ["1", "2", "3"]:
+                    name = input("\nEnter the name of the animal: ")
+                    species = input("\nEnter the species of the animal: ")
+                    weight = float(input("\nEnter the weight in lbs of the animal: "))
+                    predator = input("\nIs the animal a predator? (yes/no): ").lower() == "yes"
+                    if animal_type_choice == "1":
+                        venomous = input("\nIs the reptile venomous? (yes/no): ").lower() == "yes"
+                        return choice, name, species, weight, predator, "Reptile", venomous
+                    elif animal_type_choice == "2":
+                        little_size = int(input("\nEnter the number of puppies: "))
+                        return choice, name, species, weight, predator, "Mammal", little_size
+                    elif animal_type_choice == "3":
+                        return choice, name, species, weight, predator, "Fish", None
+                else:
+                    print("Invalid choice. Please enter 1, 2 or 3.")
+            elif choice == "2":
+                return choice, None, None, None, None, None, None
+            else:
+                print("Invalid choice. Please enter 1 or 2.")
+            
     def display_info(self):
         print(f'\nWelcome to Zoo:\n')
         if not self.habitats:
--- zoo.py
+++ zoo.py
@@ -4,39 +4,6 @@ import module_animal
 import module_reptile
 import module_mammal
 
-def get_user_choice():
-    while True:
-        zoo.display_info()
-        print("\nWhat would you like to add?")
-        print("1. Animal")
-        print("2. Habitat")
-        choice = input("\nEnter your choice (1 or 2): ")
-        if choice == "1":
-            print("\nWhat type of animal would you like to add?")
-            print("1. Reptile")
-            print("2. Mammal")
-            print("3. Fish")
-            animal_type_choice = input("\nEnter your choice (1, 2 or 3): ")
-            if animal_type_choice in ["1", "2", "3"]:
-                name = input("\nEnter the name of the animal: ")
-                species = input("\nEnter the species of the animal: ")
-                weight = float(input("\nEnter the weight in lbs of the animal: "))
-                predator = input("\nIs the animal a predator? (yes/no): ").lower() == "yes"
-                if animal_type_choice == "1":
-                    venomous = input("\nIs the reptile venomous? (yes/no): ").lower() == "yes"
-                    return choice, name, species, weight, predator, "Reptile", venomous
-                elif animal_type_choice == "2":
-                    little_size = int(input("\nEnter the number of puppies: "))
-                    return choice, name, species, weight, predator, "Mammal", little_size
-                elif animal_type_choice == "3":
-                    return choice, name, species, weight, predator, "Fish", None
-            else:
-                print("Invalid choice. Please enter 1, 2 or 3.")
-        elif choice == "2":
-            return choice, None, None, None, None, None, None
-        else:
-            print("Invalid choice. Please enter 1 or 2.")
-            
 # Crear el zoo
 zoo = module_zoo.Zoo()
 
@@ -61,7 +28,7 @@ flock.add_animal(default_mammal)
 marine.add_animal(default_fish)
 
 while True:
-    user_choice, name, species, weight, predator, animal_type, additional_info = get_user_choice()
+    user_choice, name, species, weight, predator, animal_type, additional_info = zoo.get_user_choice()
     if user_choice == "1":
         if animal_type == "Reptile":
             new_animal = module_reptile.Reptile(name, species, weight, predator, additional_info)
