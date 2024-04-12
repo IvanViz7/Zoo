cb00ab860c081ab25c24f055cf167550c96cf35d
IvanViz7 <ivanvizcarra1998@gmail.com>
2024-04-12 02:47:23 -0600

    archives .py unusefull deleted

--- module_mammal.py
+++
@@ -1,8 +0,0 @@
-import module_animal
-class Mammal(module_animal.Animal):
-    def __init__(self, name, specie, weight, predator, little_size):
-        super().__init__(name, "Mammal", specie, weight, predator)
-        self.little_size = little_size
-
-    def info(self):
-        print(f'- {self.name} is a {self.specie}, is a {self.type} and it weights {self.weight} lb. It can have {self.little_size} children.')
--- module_reptile.py
+++
@@ -1,9 +0,0 @@
-import module_animal
-class Reptile(module_animal.Animal):
-    def __init__(self, name, specie, weight, predator, venomous):
-        super().__init__(name, "Reptile", specie, weight, predator)
-        self.venomous = venomous
-
-    def info(self):
-        venom_status = "venomous" if self.venomous else "non-venomous"
-        print(f'- {self.name} is a {self.specie}, is a {self.type} and it weights {self.weight} lb. It is {venom_status}.')
